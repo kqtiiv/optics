94af94a233399de748dafe3faec465861aa77a2d
kqtiiv <katiewan.kw@gmail.com>
2025-08-02 11:02:07 +0100

    initial commit

--- main.js
+++
@@ -1,9 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const nav = document.querySelector(".nav");
-
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  nav.classList.toggle("open");
-  const expanded = hamburger.getAttribute("aria-expanded") === "true" || false;
-  hamburger.setAttribute("aria-expanded", !expanded);
-});
