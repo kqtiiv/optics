c7d4f8cb2a0b423d6f84dd48c01c68e123032861
kqtiiv <katiewan.kw@gmail.com>
2025-08-13 14:36:36 +0100

    Change icons in main menu

--- optics/src/components/simulation.tsx
+++ optics/src/components/simulation.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Box, Lock, Search, Settings, Sparkles } from "lucide-react";
+import { Sparkles } from "lucide-react";
 import { GlowingEffect } from "@/components/ui/glowing-effect";
 
 export default function Simulations() {
@@ -10,7 +10,9 @@ export default function Simulations() {
       <ul className="grid grid-cols-1 grid-rows-none gap-4 md:grid-cols-12 md:grid-rows-3 lg:gap-4 xl:max-h-[34rem] xl:grid-rows-2">
         <GridItem
           area="md:[grid-area:1/1/2/7] xl:[grid-area:1/1/2/5]"
-          icon={<Box className="h-4 w-4 text-black dark:text-neutral-400" />}
+          icon={
+            <Sparkles className="h-4 w-4 text-black dark:text-neutral-400" />
+          }
           title="Task 5: Reflection on a plane mirror"
           description="Import your own image and watch the position of the image change"
           url="/reflection-on-plane-mirror"
@@ -19,7 +21,7 @@ export default function Simulations() {
         <GridItem
           area="md:[grid-area:1/7/2/13] xl:[grid-area:2/1/3/5]"
           icon={
-            <Settings className="h-4 w-4 text-black dark:text-neutral-400" />
+            <Sparkles className="h-4 w-4 text-black dark:text-neutral-400" />
           }
           title="Task 6+7: Image from an ideal thin lens"
           description="See the image formed from an ideal thin lens when viewed from a converging lens."
@@ -28,7 +30,9 @@ export default function Simulations() {
 
         <GridItem
           area="md:[grid-area:2/1/3/7] xl:[grid-area:1/5/3/8]"
-          icon={<Lock className="h-4 w-4 text-black dark:text-neutral-400" />}
+          icon={
+            <Sparkles className="h-4 w-4 text-black dark:text-neutral-400" />
+          }
           title="Task 10: Anamorphic images"
           description="Create a mapping of pixel coordinates (that are fitted into a unit circle) to a sector of a circle with 
 radius Rf, centered at the base of the object (the red star). If you place a polished cylinder over the unit 
@@ -48,7 +52,9 @@ circle, you will create an anamorphic image. It will appear to look somewhat thr
 
         <GridItem
           area="md:[grid-area:3/1/4/13] xl:[grid-area:2/8/3/13]"
-          icon={<Search className="h-4 w-4 text-black dark:text-neutral-400" />}
+          icon={
+            <Sparkles className="h-4 w-4 text-black dark:text-neutral-400" />
+          }
           title="Task 9: Virtual image from a spherical convex mirror"
           description="See the image formed when it is reflected in a convex spherical mirror."
           url="/convex-spherical-mirror"
@@ -57,7 +63,9 @@ circle, you will create an anamorphic image. It will appear to look somewhat thr
       <ul className="grid grid-cols-1 grid-rows-none gap-4 md:grid-cols-2 lg:gap-4 xl:max-h-[34rem]">
         <GridItem
           area="md:[grid-area:1/1] sm:[grid-area:2]"
-          icon={<Box className="h-4 w-4 text-black dark:text-neutral-400" />}
+          icon={
+            <Sparkles className="h-4 w-4 text-black dark:text-neutral-400" />
+          }
           title="Task 11d: Rainbows"
           description="Model of primary and secondary rainbows 
 that you would see at sea level (with no topographic obstructions) for 
@@ -69,7 +77,7 @@ different angles of (anti) solar elevation
         <GridItem
           area="md:[grid-area:1/2] sm:[grid-area:2]"
           icon={
-            <Settings className="h-4 w-4 text-black dark:text-neutral-400" />
+            <Sparkles className="h-4 w-4 text-black dark:text-neutral-400" />
           }
           title="Task 12a: Prism"
           description="A dynamic model of the path of a beam of white light through a triangular prism."
